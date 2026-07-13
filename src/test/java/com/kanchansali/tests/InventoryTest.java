15ee0ec5b18ca3ac8a823f8b4952710dd745dcfe
Kanchan Sali <kanchansali.13197@gmail.com>
2026-07-13 22:53:56 +0530

    Multiple product Added scenario added

--- src/test/java/com/kanchansali/tests/InventoryTest.java
+++ src/test/java/com/kanchansali/tests/InventoryTest.java
@@ -40,4 +40,30 @@ public class InventoryTest extends BaseTest {
         Assert.assertEquals(inventoryPage.getCartCount(), 1);
 
     }
+
+    @Test
+
+    public void addMultipleProducts() {
+
+        LoginPage loginPage = new LoginPage(page);
+
+        loginPage.open();
+
+        InventoryPage inventoryPage =
+
+                loginPage.login("standard_user","secret_sauce");
+
+        inventoryPage.addProduct("Sauce Labs Backpack");
+
+        inventoryPage.addProduct("Sauce Labs Bike Light");
+
+        inventoryPage.addProduct("Sauce Labs Bolt T-Shirt");
+
+        Assert.assertEquals(
+
+                inventoryPage.getCartCount(),
+
+                3);
+
+    }
 }
